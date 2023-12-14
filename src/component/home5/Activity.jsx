18526b5aaa91debeff6a3774e6c119b684c9b550
Vikalp Mishra <mishravikalp896@gmail.com>
2023-12-14 16:39:28 +0530

    activity section complete

---
+++ src/component/home5/Activity.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Activity() {
+    return (
+        <div className='boxOne w-33'>
+            <div className="inner">
+                <h5 className='mt-1'>Activity</h5>
+
+                <div className="sub-heading mb-0">JAN 22</div>
+                <div className="sub-heading ">Responded to need “Volunteer Activities”</div>
+
+                <div className="sub-heading mb-0 mt-2">JAN 20</div>
+                <div className="sub-heading ">At vero eos et accusamus et iusto odio dignissimos ducimus qui deleniti atque...</div>
+                
+                <div className="sub-heading mb-0 mt-2">JAN 18</div>
+                <div className="sub-heading ">Completed to need this and must be do that in this</div>
+
+                <div className="sub-heading mb-0 mt-2">PROJECT STARTS</div>
+
+                <div className="btn mt-4">Load More</div>
+            </div>
+        </div>
+    )
+}
