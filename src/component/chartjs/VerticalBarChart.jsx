2908123c4774f4773b73d696a37af22d58809d55
Vikalp Mishra <mishravikalp896@gmail.com>
2023-12-20 11:48:46 +0530

    chartjs section complete

---
+++ src/component/chartjs/VerticalBarChart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function VerticalBarChart() {
+  return (
+    <div className='mt-70 boxOne w-50'>
+      <div className="inner"></div>
+    </div>
+  )
+}
+
+
