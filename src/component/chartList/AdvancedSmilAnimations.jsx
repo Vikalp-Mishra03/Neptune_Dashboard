118daee7f1cc956f0851d25246fc8b0ef10f0aea
Vikalp Mishra <mishravikalp896@gmail.com>
2023-12-20 11:49:09 +0530

    chartList section complete

---
+++ src/component/chartList/AdvancedSmilAnimations.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+// export default function AdvancedSmilAnimations() {
+//     return (
+//         <div className='boxOne mt-70 w-50 '>
+//             <div className="inner">
+//                 <h5>Advanced Smil Animations</h5>
+//             </div>
+//         </div>
+//     )
+// }
+
+
+
---
+++ src/component/chartList/SVGPathAnimations.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function SVGPathAnimations() {
+    return (
+        <div className='boxOne mt-70 w-50'>
+            <div className="inner">
+                <h5>SVG Path Animations</h5>
+                <div id="chart"></div>
+            </div>
+        </div>
+    )
+}
